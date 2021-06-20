f16052d71d18dfb48b0074d3770859eba091637b
thereyner <33945338+pizza-danger@users.noreply.github.com>
2021-06-20 13:23:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let superChimpOne = {
+    name: "Chad",
+    species: "Chimpanzee",
+    mass: 9,
+    age: 6
+};
+
+let salamander = {
+    name: "Lacey",
+    species: "Axolotl Salamander",
+    mass: 0.1,
+    age: 5
+};
+
+let chimpanzeeTwo = {
+  name: "Brad",
+  species: "Chimpanzee",
+  mass: 11,
+  age: 6
+};
+
+let beagle = {
+  name: "Leroy",
+  species: "Beagle",
+  mass: 14,
+  age: 5,
+};
+
+let tardigrade = {
+  name: "Almina",
+  species: "Tardigrade",
+  mass: 0.0000000001,
+  age: 1,
+};
+
+
+// After you have created the other object literals, add the astronautID property to each one.
+
+// Create an array to hold the animal objects.
+
+// Print out the relevant information about each animal.
+
+// Start an animal race!
