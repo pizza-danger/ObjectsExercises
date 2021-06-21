c02fb32afd78a557a14db10075d0090d2ca3f52f
thereyner <33945338+pizza-danger@users.noreply.github.com>
2021-06-21 12:43:16 +0000

    Added Astronaut ID. The original 2, SuperChimpOne and Salamander, were a problem. Comment between them.

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let superChimpOne = {
     mass: 9,
     age: 6
 };
-
+//
 let salamander = {
-    name: "Lacey",
-    species: "Axolotl Salamander",
-    mass: 0.1,
-    age: 5
+  name: "Lacey",
+  species: "Axolotl Salamander",
+  mass: 0.1,
+  age: 5
 };
 
 let chimpanzeeTwo = {
@@ -32,10 +32,25 @@ let tardigrade = {
   mass: 0.0000000001,
   age: 1,
 };
-
-
-
+let astronautAnimals = [superChimpOne, salamander, chimpanzeeTwo, beagle, tardigrade];
+let idIndex = 0;
 // After you have created the other object literals, add the astronautID property to each one.
+
+for (let i = 1; i < astronautAnimals.length; i++) {
+  idIndex += 1;
+  astronautAnimals[i].astronautID = idIndex;
+}
+console.log(salamander);
+// superChimpOne["astronautID"] = Math.round(Math.random()*10);
+// salamander["astronautID"] = Math.round(Math.random()*10);
+// chimpanzeeTwo["astronautID"] = Math.round(Math.random()*10);
+// beagle["astronautID"] = Math.round(Math.random()*10);
+// tardigrade["astronautID"] = Math.round(Math.random()*10);
+// console.log(superChimpOne.astronautID, superChimpOne.name);
+// console.log(salamander.astronautID, salamander.name);
+// console.log(chimpanzeeTwo.astronautID, chimpanzeeTwo.name);
+// console.log(beagle.astronautID, beagle.name);
+// console.log(tardigrade.astronautID, tardigrade.name);
 
 // Create an array to hold the animal objects.
 
